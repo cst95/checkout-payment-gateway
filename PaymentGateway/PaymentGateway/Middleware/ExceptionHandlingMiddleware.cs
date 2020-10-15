e0c9ce235d0e3b178ccc0aeba92c6606eb73643c
Charlie Stokes <cstokes873@gmail.com>
2020-10-15 20:58:19 +0100

    Add and register new global exception handling middleware.

---
+++ PaymentGateway/PaymentGateway/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using PaymentGateway.Models;
+
+namespace PaymentGateway.Middleware
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError("Unhandled exception occured. Message {Message}. Exception {Exception}", exception.Message, exception);
+
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+                
+                var response = new ApiException(context.Response.StatusCode, "Internal Server Error");
+                var jsonResponse = JsonSerializer.Serialize(response, new JsonSerializerOptions{PropertyNamingPolicy = JsonNamingPolicy.CamelCase});
+
+                await context.Response.WriteAsync(jsonResponse);
+            } 
+        }
+    }
+}
---
+++ PaymentGateway/PaymentGateway/Models/ApiException.cs
@@ -0,0 +1,14 @@
+﻿namespace PaymentGateway.Models
+{
+    public class ApiException
+    {
+        public ApiException(int statusCode, string message)
+        {
+            StatusCode = statusCode;
+            Message = message;
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; }    
+    }
+}
